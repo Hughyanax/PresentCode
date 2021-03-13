7d57b8382d7b19b31a8bcfe13b247d9b2c043788
Hughyanax <892621636@qq.com>
2021-03-13 12:31:14 +0800

    failed

---
+++ main.cs
@@ -0,0 +1,14 @@
+using system;
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.writeline("Hello World");
+            Console.readkey();
+        }
+
+    }
+}
+
